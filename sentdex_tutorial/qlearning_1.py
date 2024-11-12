897857c06e3173d0d1dabecbebf399fc290a0c9e
debrupf2946 <f20212946@goa.bits-pilani.ac.in>
2024-11-12 18:18:06 +0530

    added qlearning method with epsilon greedy training

---
+++ sentdex_tutorial/qlearning_1.py
@@ -0,0 +1,77 @@
+import gym
+import numpy as np
+
+LEARNING_RATE=0.1
+DISCOUNT=0.95
+EPISODES=25000
+SHOW_EVERY=2000
+
+
+epsilon=1
+START_EPSILON_DECAYING = 1
+END_EPSILON_DECAYING = EPISODES//2
+epsilon_decay_value = epsilon/(END_EPSILON_DECAYING - START_EPSILON_DECAYING)
+
+def get_descrete_state(state):
+    discrete_state=(state-env.observation_space.low)/discrete_os_win_size
+    return tuple(discrete_state.astype(np.int))
+
+env=gym.make("MountainCar-v0",render_mode="human")
+
+env.reset()
+
+done=False
+
+
+
+DISCRETE_OS_SIZE=[20]*len(env.observation_space.high)
+discrete_os_win_size=(env.observation_space.high-env.observation_space.low)/DISCRETE_OS_SIZE
+
+q_table=np.random.uniform(low=-2,high=0,size=(DISCRETE_OS_SIZE+[env.action_space.n]))
+# print(q_table.shape)
+
+discrete_state=get_descrete_state(env.reset()[0])
+
+
+for episodes in range(EPISODES):
+    
+    if episodes % SHOW_EVERY==0:
+        print(episodes)
+        render=True
+    else:
+        render=False
+        
+    discrete_state=get_descrete_state(env.reset()[0])
+    done=False
+    
+    while not done:
+        if np.random.random()>epsilon:
+            action=np.argmax(q_table[discrete_state])
+        else:
+            action=np.random.randint(0,env.action_space.n)
+            
+        new_state,reward,done,truncated,info=env.step(action)
+        new_discrete_state=get_descrete_state(new_state)
+        if render:
+            env.render()
+        if not done:
+            max_feature_q=np.max(q_table[new_discrete_state])
+            current_q=q_table[discrete_state+(action,)]
+            
+            new_q=(1-LEARNING_RATE)*current_q + LEARNING_RATE*(reward + DISCOUNT*max_feature_q)
+            
+            q_table[discrete_state+(action,)]=new_q
+            
+        elif new_state[0]>=env.goal_position:
+            print(f"We Made it on episode {episodes}")
+            q_table[discrete_state+(action,)]=0
+            
+        discrete_state=new_discrete_state
+        
+    if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
+        epsilon -= epsilon_decay_value
+    
+env.close()
+
+
+
